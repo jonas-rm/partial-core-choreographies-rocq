6fd08f3d413ec12f518a33418e57ffb19141bc5e
Luís Cruz-Filipe <lcfilipe@gmail.com>
2019-02-22 14:31:59 +0100

    * now with parallelism
    I actually belive composition was the hard nut...

--- Implementation.v
+++ Implementation.v
@@ -25,14 +25,21 @@ induction p.
   apply IHp; eapply max_lt_r; exact H.
 Qed.
 
-(** Vector containing the numbers 1 to n. *)
-Fixpoint vec_1_to_n_aux n k : t nat n :=
+(** Vector containing the numbers k to k+n. *)
+Fixpoint vec_k_to_n n k : t nat n :=
   match n with
   | 0 => []
-  | S m => k :: vec_1_to_n_aux m (S k)
+  | S m => k :: vec_k_to_n m (S k)
   end.
 
-Definition vec_1_to_n n : t nat n := vec_1_to_n_aux n 1.
+Definition vec_1_to_n n : t nat n := vec_k_to_n n 1.
+
+(** Vector of vectors with values [[m; ...; m+n-1] [m+n; ...; m+2n-1] ... [m+(k-1)n; ...; m+kn-1]]. *)
+Fixpoint vec_m_with_k m k n :=
+  match k with
+  | 0 => []
+  | S k' => (vec_k_to_n n m :: vec_m_with_k (m+n) k' n)
+  end.
 
 (** Sum of a vector of natural numbers. *)
 Fixpoint vsum {n} (v:t nat n) :=
@@ -110,23 +117,22 @@ Fixpoint skip_labels (n:nat) {k} {m} (fs:t (PRFunction m) k) : t Pid k :=
     - target is the output process for the first function to implement
     - init is the label l_i
 *)
-Fixpoint compose_args {m} {k} (fs:t (PRFunction m) k) d (Hd:forall i, depth fs[@i] < d) (ps:t Pid m) (target:nat) (init:nat)
+Fixpoint seq_compose {m} {k} (fs:t (PRFunction m) k) d (Hd:forall i, depth fs[@i] < d) (ps:t Pid m) (target init:nat)
   (Implement : forall m' (f:PRFunction m') (Hd:depth f < d) (ps':t Pid m') (q' i':nat), Choreography) {struct fs} : Choreography.
-Proof.
-destruct fs.
-- apply End.
-- apply (fatsemi (Implement m h (Hd Fin.F1) ps target init)).
-  assert (forall i, depth fs[@i] < d).
-  intro; apply (Hd (Fin.FS i)).
-  apply (compose_args _ _ fs d H ps (S target) (init + Pi h) Implement).
-Defined.
-
 (*
   match fs with
   | [] => End
   | f :: fs') => Implement m f d (Hd Fin.F1) ps target init ;; compose_args fs' ps (S target) (init + Pi f) Implement
   end.
 *)
+Proof.
+destruct fs.
+- apply End.
+- apply (fatsemi (Implement m h (Hd Fin.F1) ps target init)).
+  assert (forall i, depth fs[@i] < d).
+  intro; apply (Hd (Fin.FS i)).
+  apply (seq_compose _ _ fs d H ps (S target) (init + Pi h) Implement).
+Defined.
 
 (** Relationship to the arguments in the paper:
     - f is the function (f)
@@ -156,7 +162,7 @@ induction d.
     assert (forall i, depth fs[@i] < d).
     intros; rewrite <- nth_map'; apply vmax_lt; auto.
     apply
-    (compose_args fs _ H ps init (init+k) (fun m f => Implementation_aux m f d);;
+    (seq_compose fs _ H ps init (init+k) (fun m f => Implementation_aux m f d);;
       Implementation_aux _ f _ Hdf (seq_labels init fs) q (init + (vsum (map Pi fs)))).
 
   (* Recursion *)
@@ -178,6 +184,94 @@ Definition Implementation' {m} (f:PRFunction m) : Choreography :=
 (* Sanity checks.
 Eval compute in (Implementation' (Composition Successor [Zero])).
 Eval compute in (Implementation' (Composition Zero [Projection aux13])).
+Eval compute in (Implementation' (Composition (Projection aux22) (Zero :: [Successor]))).
+*)
+
+(** There is also a parallel variant for composition. This is defined in the same steps, but
+    requires yet another auxiliary function. *)
+
+Definition copy_input {m} (ps qs:t Pid m) : Choreography.
+(*
+  match m with
+  | 0 => End
+  | S _ => (hd ps # this --> hd qs; copy_input (tl ps) (tl qs))
+  end.
+*)
+Proof.
+induction m.
++ apply End.
++ apply (hd ps # this --> hd qs; IHm (tl ps) (tl qs)).
+Defined.
+
+Definition copy_input_iter {m} (ps:t Pid m) {n} (qs: t (t Pid m) n): Choreography.
+Proof.
+induction n.
++ apply End.
++ apply (copy_input ps (hd qs) ;; IHn (tl qs)).
+Defined.
+
+(** The extra argument ps_start is for the processes where the inputs in fs[@0] come from. *)
+Fixpoint par_compose {m} {k} (fs:t (PRFunction m) k) d (Hd:forall i, depth fs[@i] < d) (target init ps_start:nat)
+  (Implement : forall m' (f:PRFunction m') (Hd:depth f < d) (ps':t Pid m') (q' i':nat), Choreography) {struct fs} : Choreography.
+Proof.
+destruct fs.
+- apply End.
+- apply (fatsemi (Implement m h (Hd Fin.F1) (vec_k_to_n m ps_start) target init)).
+  assert (forall i, depth fs[@i] < d).
+  intro; apply (Hd (Fin.FS i)).
+  apply (par_compose _ _ fs d H (S target) (init + Pi h) (ps_start + m) Implement).
+Defined.
+
+Fixpoint Par_Implementation_aux {m} (f:PRFunction m) d (Hd:depth f<d) (ps:t Pid m) (q:Pid) (init:nat) {struct d}: Choreography.
+induction d.
++ elim (Nat.nlt_0_r _ Hd).
++ destruct f; intros.
+
+  (* Zero *)
+  - apply (ps[@Fin.F1] # zero --> q; End).
+
+  (* Successor *)
+  - apply (ps[@Fin.F1] # succ_this --> q; End).
+
+  (* Projection *)
+  - apply (ps[@Fin.of_nat_lt l] # this --> q; End).
+
+  (* Composition *)
+  - simpl in Hd; generalize (lt_S_n _ _ Hd); clear Hd; intro Hd'.
+    generalize (max_lt_l _ _ _ Hd').
+    generalize (max_lt_r _ _ _ Hd').
+    intros Hdfs Hdf.
+    assert (forall i, depth fs[@i] < d).
+    intros; rewrite <- nth_map'; apply vmax_lt; auto.
+    set (init' := init + m*k).
+    apply
+    (copy_input_iter ps (vec_m_with_k init m k) ;;
+      par_compose fs _ H init' (init'+k) init (fun m f => Par_Implementation_aux m f d);;
+      Par_Implementation_aux _ f _ Hdf (seq_labels init' fs) q (init' + (vsum (map Pi fs)))).
+
+  (* Recursion *)
+  - apply End.
+
+  (* Minimization *)
+  - apply End.
+Defined.
+
+Definition Par_Implementation {m} (f:PRFunction m) (ps:t Pid m) (q:Pid) : Choreography :=
+  Par_Implementation_aux f _ (lt_n_Sn (depth f)) ps q (S (max q (vmax ps))).
+
+Definition Par_Implementation' {m} (f:PRFunction m) : Choreography :=
+  Par_Implementation f (vec_1_to_n m) 0.
+
+(* Sanity checks.
+Eval compute in (Implementation' (Composition Successor [Zero])).
+Eval compute in (Par_Implementation' (Composition Successor [Zero])).
+Eval compute in (Implementation' (Composition Zero [Projection aux13])).
+Eval compute in (Par_Implementation' (Composition Zero [Projection aux13])).
+Eval compute in (Implementation' (Composition (Projection aux22) (Zero :: [Successor]))).
+Eval compute in (Par_Implementation' (Composition (Projection aux22) (Zero :: [Successor]))).
 *)
 
 End Definitions.
+
+(* We could also prove the converse: every choreography computes a computable function.
+   At least without recursion... *)
