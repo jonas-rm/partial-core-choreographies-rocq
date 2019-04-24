5f3f278d900b4878f050210b38fe6a5f8d86b7d2
Luís Cruz-Filipe <lcfilipe@gmail.com>
2019-04-24 14:19:03 +0200

    * compilation fix

--- Implementation.v
+++ Implementation.v
@@ -147,13 +147,15 @@ Fixpoint single_exit_point (C:Choreography) : Prop :=
 Lemma fatsemi_precongr : forall C C1 C2, C1 ~<= C2 -> (C1;;C) ~<= (C2;;C).
 intros; induction H.
 + apply Refl.
-+ apply Trans with (C2;;C); auto.
-+ apply EtaEta; auto.
-+ apply EtaCond; auto.
-+ apply CondEta; auto.
-+ apply CondCond; auto.
-+ apply CtxEta; auto.
-+ apply CtxCond; auto.
++ apply Trans with (C2;;C); auto; clear C3 H0 IHPrecongr.
+  induction H.
+  - apply EtaEta; auto.
+  - apply EtaCond; auto.
+  - apply CondEta; auto.
+  - apply CondCond; auto.
+  - apply CtxEta; auto.
+  - apply CtxThen; auto.
+  - apply CtxElse; auto.
 Qed.
 
 Lemma fatsemi_End : forall C, (C;;End) = C.
