2f0700a432a26ae252463d85947c90d7d02496cb
Fabrizio Montesi <famontesi@gmail.com>
2020-05-20 18:59:18 +0200

    CIAO

--- EPP.v
+++ EPP.v
@@ -15,91 +15,44 @@ Module Export CSt := GState P V X.
 Section MaybeMove.
 Definition Pid_In_dec := in_dec P.eq_dec.
 
-Print Behaviour_ind.
-Print Behaviour.
-Scheme Behaviour_rec := Induction for Behaviour Sort Type.
-Print Behaviour_rec.
-
-Definition option_apply_or_True {A B:Type} (f:Behaviour -> Type) (o:option Behaviour) : Type :=
+(*
+Definition option_apply_or_True (f:Behaviour -> Type) (o:option Behaviour) : Type :=
 match o with
 | Some B => f B
 | _ => True
 end.
-
-(*
-Definition Behaviour_rec_2 :
-forall P : Behaviour -> Type,
-  P bnil%SP ->
-  (forall (p : Pid) (e : Expr) (b : Behaviour), P b -> P (p ! e; b)%SP) ->
-  (forall (p : Pid) (v : Var) (b : Behaviour), P b -> P (p ? v; b)%SP) ->
-  (forall (p : Pid) (l : Label) (b : Behaviour), P b -> P (p (+) l; b)%SP) ->
-  (forall (p : Pid) (o : Label -> option Behaviour),
-    option_apply_or_True P (o left) -> option_apply_or_True P (o right) ->
-    P (p & o)%SP
-  ) ->
-  (forall (b : BExpr) (b0 : Behaviour),
-  P b0 -> forall b1 : Behaviour, P b1 -> P (If b Then b0 Else b1)%SP) ->
-  (forall r : RecVar, P (Call r)) -> forall b : Behaviour, P b :=
-fun (P : Behaviour -> Type) (f : P bnil%SP)
-  (f0 : forall (p : Pid) (e : Expr) (b : Behaviour), P b -> P (p ! e; b)%SP)
-  (f1 : forall (p : Pid) (v : Var) (b : Behaviour), P b -> P (p ? v; b)%SP)
-  (f2 : forall (p : Pid) (l : Label) (b : Behaviour), P b -> P (p (+) l; b)%SP)
-  (f3 : forall (p : Pid) (o : Label -> option Behaviour),
-    option_apply_or_True P (o left) -> option_apply_or_True P (o right) ->
-    P (p & o)%SP
-  )
-  (f4 : forall (b : BExpr) (b0 : Behaviour),
-        P b0 -> forall b1 : Behaviour, P b1 -> P (If b Then b0 Else b1)%SP)
-  (f5 : forall r : RecVar, P (Call r)) =>
-fix F (b : Behaviour) : P b :=
-  match b as b0 return (P b0) with
-  | bnil%SP => f
-  | (p ! e; b0)%SP => f0 p e b0 (F b0)
-  | (p ? v; b0)%SP => f1 p v b0 (F b0)
-  | (p (+) l; b0)%SP => f2 p l b0 (F b0)
-  | (p & o)%SP => f3 p o (option_apply_or_True F (o left)) (option_apply_or_True F (o right))
-  | (If b0 Then b1 Else b2)%SP => f4 b0 b1 (F b1) b2 (F b2)
-  | Call r => f5 r
-  end
-.
 *)
 
-Check Behaviour_rec.
+Definition option_apply_or_True (f:Behaviour -> Prop) (o:option Behaviour) : Prop :=
+  forall B, o = Some B -> f B
+  /\
+  o = None -> True.
 
-(*
-Definition Behaviour_ind_b :
-  forall P : Behaviour -> Type,
-    P bnil%SP ->
-    (forall (p : Pid) (e : Expr) (b : Behaviour), P b -> P (p ! e; b)%SP) ->
-    (forall (p : Pid) (v : Var) (b : Behaviour), P b -> P (p ? v; b)%SP) ->
-    (forall (p : Pid) (l : Label) (b : Behaviour), P b -> P (p (+) l; b)%SP) ->
-    (forall (p : Pid) (o : Label -> option Behaviour),
-      forall B1, o left = Some B1 -> P B1 -> forall B2, o right = Some B2 -> P B2 ->
-      P (p & o)%SP
-    ) ->
-    (forall (b : BExpr) (b0 : Behaviour),
-    P b0 -> forall b1 : Behaviour, P b1 -> P (If b Then b0 Else b1)%SP) ->
-    (forall r : RecVar, P (Call r)) -> forall b : Behaviour, P b :=
-match b with
-  | bnil%SP => f
-  | (p ! e; b0)%SP => f0 p e b0 (F b0)
-  | (p ? v; b0)%SP => f1 p v b0 (F b0)
-  | (p (+) l; b0)%SP => f2 p l b0 (F b0)
-  | (p & o)%SP => f3 p o
-  | (If b0 Then b1 Else b2)%SP => f4 b0 b1 (F b1) b2 (F b2)
-  | Call r => f5 r
-  end
-.
-*)
+Fixpoint depth (B:Behaviour) : nat :=
+match B with
+ | Send p e B' => 1 + depth B'
+ | Recv p x B' => 1 + depth B'
+ | Sel p l B' => 1 + depth B'
+ | Branching p f => 1 +
+    match f left, f right with
+     | Some B1, Some B2 => Nat.max (depth B1) (depth B2)
+     | Some B1, None => depth B1
+     | None, Some B2 => depth B2
+     | None, None => 0
+    end
+ | Cond e B1 B2 => 1 + Nat.max (depth B1) (depth B2)
+ | Call X => 1
+ | End => 1
+end.
 
 Theorem Behaviour_ind_b :
-  forall P : Behaviour -> Type,
+  forall P : Behaviour -> Prop,
     P bnil%SP ->
     (forall (p : Pid) (e : Expr) (b : Behaviour), P b -> P (p ! e; b)%SP) ->
     (forall (p : Pid) (v : Var) (b : Behaviour), P b -> P (p ? v; b)%SP) ->
     (forall (p : Pid) (l : Label) (b : Behaviour), P b -> P (p (+) l; b)%SP) ->
     (forall (p : Pid) (o : Label -> option Behaviour),
-      forall B1, o left = Some B1 -> P B1 -> forall B2, o right = Some B2 -> P B2 ->
+      (forall (l:Label), option_apply_or_True P (o l)) ->
       P (p & o)%SP
     ) ->
     (forall (b : BExpr) (b0 : Behaviour),
@@ -107,9 +60,51 @@ Theorem Behaviour_ind_b :
     (forall r : RecVar, P (Call r)) -> forall b : Behaviour, P b.
 Proof.
 intros.
-destruct b; auto.
-+ specialize (X0 p e b).
-Admitted.
+revert b.
+assert (forall d b, depth b <= d -> P b).
+2: eauto.
+induction d; intros; case_eq b; intros; auto; rewrite H7 in H6; try (exfalso; inversion H6; fail).
++ apply H0. apply IHd. simpl in H. auto with arith.
++ apply H1. apply IHd. simpl in H. auto with arith.
++ apply H2. apply IHd. simpl in H. auto with arith.
++ apply H3. intro.
+  case_eq (o l); simpl; intros; auto.
+  2: easy.
+  red.
+  intros; auto.
++ apply H4; apply IHd; simpl in H; apply le_S_n in H6.
+  - etransitivity. eapply Nat.le_max_l. eauto.
+  - etransitivity. eapply Nat.le_max_r. eauto.
+(*
+induction d; intros; case_eq b; intros; auto; rewrite H0 in H; try (exfalso; inversion H; fail).
++ apply X0. apply IHd. simpl in H. auto with arith.
++ apply X1. apply IHd. simpl in H. auto with arith.
++ apply X2. apply IHd. simpl in H. auto with arith.
++ apply X3. intro.
+  case_eq (o l); simpl; auto.
+  intros.
+  apply IHd. simpl in H.
+  revert H.
+  induction l.
+  - rewrite H1.
+    case (o right); auto with arith.
+    intros.
+    apply le_S_n in H.
+    etransitivity.
+    eapply Nat.le_max_l.
+    eauto.
+  - rewrite H1.
+    case (o left); auto with arith.
+    intros.
+    apply le_S_n in H.
+    etransitivity.
+    eapply Nat.le_max_r.
+    eauto.
++ apply X4; apply IHd; simpl in H; apply le_S_n in H.
+  - etransitivity. eapply Nat.le_max_l. eauto.
+  - etransitivity. eapply Nat.le_max_r. eauto.
+*)
+Qed.
 
 End MaybeMove.
 
@@ -274,11 +269,8 @@ end.
 
 Definition more_branches_beh (B1 B2:Behaviour) := (merge_beh B1 B2) = Some B1.
 
-Print Behaviour_ind.
-
 Lemma more_branches_beh_char_1 : forall (B1 B2:Behaviour), more_branches_beh B1 B2 -> more_branches_beh_direct B1 B2.
-induction B1, B2; intros; inversion H; auto.
-+ simpl; trivial.
+induction B1 using Behaviour_ind_b; induction B2 using Behaviour_ind_b; intros; try inversion H; try easy.
 + simpl.
   case_eq (Pid_dec p p0); case_eq (Expr_dec e e0); intros; rewrite H0 in H1; rewrite H2 in H1; simpl in H1; simpl; inversion H1.
   specialize (IHB1 B2).
@@ -306,7 +298,44 @@ induction B1, B2; intros; inversion H; auto.
     rewrite H5 in IHB1.
     auto.
   - inversion H4.
-+
++ case_eq (Pid_dec p p0); case_eq (o left); case_eq (o0 left); case_eq (o right); case_eq (o0 right); intros; auto.
+  - case_eq (more_branches_beh b b0).
+
+red in H.
+    
+  all: rewrite H2, H3, H4, H5 in H1.
+  - pose (Hl := (H left)).
+    rewrite H6 in Hl.
+    red in Hl.
+    split.
+    apply Hl.
+ case_eq (merge_beh b2 b1); case_eq (merge_beh b0 b); intros.
+    * rewrite H6, H7 in H1.
+  
+2: rewrite H2 in H1; simpl in H1; inversion H1.
+  
+    
+  pose (Hl := (X left)).
+
+  destruct (o left); destruct (o0 left); destruct (o right); destruct (o0 right); auto.
+  2: {
+    split; auto.
+
+    
+  split.
+  - destruct (o left); destruct (o0 left); auto.
+    2: {
+simpl in H1.
+    2: {
+      destruct (o0 left); auto.
+      intros.
+      
+
+ case (o left); case (o0 left); auto.
+    * case (o0 left); auto.
+      intros.
+      apply X0.
+  
 (* No induction hypothesis for branchings.. *)
 (* simpl.
   case_eq (Pid_dec p p0); intros; rewrite H0 in H1.
