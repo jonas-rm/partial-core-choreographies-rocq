c443258fda95cb85977532d9c9eacf19a832b04c
Luís Cruz-Filipe <lcf@imada.sdu.dk>
2021-02-09 17:00:08 +0100

    * this should not be here

--- Draft.v
+++
@@ -1,8 +0,0 @@
-(* PLAN
- - product lattice and approximator
-   Prove: properties of approximator, wffp, construction by wf induction
- - O-induction: x: beta -> L
-    x_(S a) is O-refinement: x_a <= x_(S a) <= O(x_a)
-    x_lim is lub of lower
-   Prove: all terminal O-inductions have the same limit.
-*)
