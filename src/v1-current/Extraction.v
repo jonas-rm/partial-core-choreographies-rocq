5a270d9f658e4c4a280a0313615169e88c6a09d9
lovrosdu <lugovic@imada.sdu.dk>
2023-01-19 12:49:56 +0100

    Update Extraction

--- src/v1-current/Extraction.v
+++ src/v1-current/Extraction.v
@@ -1,12 +1,33 @@
 Require Export Implementation.
 Require Export EPP.
-
 Require Extraction.
 
-Extraction Language Haskell.
+Definition sig := Build_Signature CC_Nat Bool CC_Nat CC_Expressions Bool_Expressions CC_Nat Unit CC_Eval CC_BEval.
+Definition epp' := @EPP.epp sig.
 
-Extraction "test" EPP.epp.
+(* Extraction Inline Signature. *)
+(* Extraction Inline Network. *)
+(* Extraction Inline pid. *)
+(* Extraction Inline var. *)
+(* Extraction Inline value. *)
+(* Extraction Inline expr. *)
+(* Extraction Inline bexpr. *)
+(* Extraction Inline recvar. *)
+(* Extraction Inline ann. *)
+(* Extraction Inline ev. *)
+(* Extraction Inline bev. *)
+(* Extraction Inline bproj_dec. *)
+(* Extraction Inline epp_C. *)
+(* Extraction Inline epp_D. *)
+(* Extraction Inline epp. *)
+(* Extraction Inline sig. *)
+(* Extraction Inline merge_dec. *)
+
+Extraction Language Haskell.
+Extraction "EPP" epp'.
+
+Extraction Language Scheme.
+Extraction "EPP" epp'.
 
 Extraction Language OCaml.
-
-Extraction "test" EPP.epp.
+Extraction "EPP" epp'.
